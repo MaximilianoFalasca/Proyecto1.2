fbfa5c0af414460572c8c6acaab2c6d86615628a
MaximilianoFalasca <maximilianofalasca18@gmail.com>
2025-05-29 17:03:46 -0300

    a

--- frontend/app/(dashboard)/reserva/page.tsx
+++ frontend/app/(dashboard)/reserva/page.tsx
@@ -8,6 +8,7 @@ import { Box, Button } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Divider from '@mui/material/Divider';
 import { blue } from '@mui/material/colors';
+import { Grid2 } from '@mui/material';
 
 interface Asiento{
   numero: number;
@@ -184,7 +185,7 @@ export default function ReservaPage() {
       )
         
       */
-
+    /*
     let rows = []  
 
     let key = 0
@@ -210,13 +211,137 @@ export default function ReservaPage() {
       )
     }
   
+    */
+    /*
+    const rowsCol1 = []
+    const rowsCol2 = []
+
+
+    let key = 0
+
+    // hacerlo todo en una sola columna y despues asignar a los de la mitad mas espacio que los otros (se la pantalla 
+    // se divide en 20 dales a los de los extremos 4 y a los del medio 8 y a estos ultimos asignarle que vayan a los extremos)
+
+
+    for (let index = 3; index < asientos.length; index+=4) {
+      rowsCol1.push(
+        <div key={key++} style={{ display: 'flex', flexDirection: 'row'}} >
+          <IconButton 
+            key={index-3} 
+            disabled={asientos[index-3].estado=="inhabilitado" || asientos[index-3].estado=="ocupado"} 
+            disableRipple={true} 
+            data-numero={asientos[index-3].numero} 
+            data-matricula={asientos[index-3].matricula} 
+            data-precio={asientos[index-3].precio} 
+            onClick={(e) => seleccionAsiento(asientos[index-3])}
+          >
+            <CustomIcon 
+              color={asientos[index-3].estado=="libre"?"disabled":asientos[index-3].estado=="inhabilitado" || asientos[index-3].estado=="ocupado"?"error":"primary"}
+              fontSize='large'
+            />
+          </IconButton>
+          <IconButton 
+            key={index-2} 
+            disabled={asientos[index-2].estado=="inhabilitado" || asientos[index-2].estado=="ocupado"} 
+            disableRipple={true} 
+            data-numero={asientos[index-2].numero} 
+            data-matricula={asientos[index-2].matricula} 
+            data-precio={asientos[index-2].precio} 
+            onClick={(e) => seleccionAsiento(asientos[index-2])}
+          >
+            <CustomIcon 
+              color={asientos[index-2].estado=="libre"?"disabled":asientos[index-2].estado=="inhabilitado" || asientos[index-2].estado=="ocupado"?"error":"primary"}
+              fontSize='large'
+            />
+          </IconButton>
+        </div>
+      )
+      rowsCol2.push(
+        <div key={key++} style={{ display: 'flex', flexDirection: 'row'}} >
+          <IconButton 
+            key={index-1} 
+            disabled={asientos[index-1].estado=="inhabilitado" || asientos[index-1].estado=="ocupado"} 
+            disableRipple={true} 
+            data-numero={asientos[index-1].numero} 
+            data-matricula={asientos[index-1].matricula} 
+            data-precio={asientos[index-1].precio} 
+            onClick={(e) => seleccionAsiento(asientos[index-1])}
+          >
+            <CustomIcon 
+              color={asientos[index-1].estado=="libre"?"disabled":asientos[index-1].estado=="inhabilitado" || asientos[index-1].estado=="ocupado"?"error":"primary"}
+              fontSize='large'
+            />
+          </IconButton>
+          <IconButton 
+            id={`${index}`}
+            key={index} 
+            disabled={asientos[index].estado=="inhabilitado" || asientos[index].estado=="ocupado"} 
+            disableRipple={true} data-numero={asientos[index].numero} 
+            data-matricula={asientos[index].matricula} 
+            data-precio={asientos[index].precio} 
+            onClick={(e) => {
+              console.log(e.currentTarget) 
+              seleccionAsiento(asientos[index])
+            }}
+            component="button"
+          >
+            <CustomIcon 
+              color={asientos[index].estado=="libre"?"disabled":asientos[index].estado=="inhabilitado" || asientos[index].estado=="ocupado"?"error":"primary"}
+              fontSize='large'
+            />
+          </IconButton>
+        </div>
+      )
+    }
+    */
+
+    /*
+    de este modo tiene que quedar
+    tengo que ver una manera de identificar a los del medio, lo puedo hacer viendo que si los primeros dos digitos en ascii son
+    10 o 11 entonces son los del medio y si son 00 o 01 son los de los extremos, tengo que ver como hacer calculos en base a esto
+    
+      <Grid container spacing={2} minHeight={160}>
+        <Grid display="flex" justifyContent="center" alignItems="center" size={3}>
+          <Avatar src="/static/images/avatar/1.jpg" />
+        </Grid>
+        <Grid display="flex" justifyContent="start" alignItems="center" size={3}>
+          <Avatar src="/static/images/avatar/1.jpg" />
+        </Grid>
+        <Grid display="flex" justifyContent="end" alignItems="center" size={3}>
+          <Avatar src="/static/images/avatar/2.jpg" />
+        </Grid>
+        <Grid display="flex" justifyContent="center" alignItems="center" size={3}>
+          <Avatar src="/static/images/avatar/3.jpg" />
+        </Grid>
+      </Grid>
+    */
+    const getJustify = (codigo: number) => {
+      const lastTwoBits = codigo & 0b11;
+      if (lastTwoBits === 0b10 || lastTwoBits === 0b11) return 'center';
+      if (lastTwoBits === 0b00) return 'start';
+      if (lastTwoBits === 0b01) return 'end';
+      return 'center';
+    };
 
     return (
-      <Box style={{ display: 'flex', justifyContent: 'space-around', alignItems:'center', flex: 1 }}>
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
-          {rows}
-        </div>
-      </Box>
+      <Grid2 container spacing={2} minHeight={160}>
+        {asientos.map((asiento, index) => (
+          <Grid2
+            key={index}
+            size={{ xs: 3, sm: 4 }}
+            display="flex"
+            justifyContent={getJustify(index)}
+            alignItems="center"
+            component="svg"
+            onClick={(e) => seleccionAsiento(asiento)}
+          >
+            <CustomIcon 
+              color={asiento.estado=="libre"?"disabled":asiento.estado=="inhabilitado" || asiento.estado=="ocupado"?"error":"primary"}
+              fontSize='large'
+            />
+          </Grid2>
+        ))}
+      </Grid2>
     )
   }
 
@@ -274,6 +399,14 @@ export default function ReservaPage() {
     <Box style={{ display: 'flex', justifyContent: 'space-around', alignItems:'center', height: '100vh', width:'100%', flex: 1}}>
       {obtenerObjetosAsientos()}
       {mostrarVuelo()}
+      <Box style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        <Button variant="outlined" onClick={() => console.log("confirmar")}>
+          Reservar
+        </Button>
+        <Button variant="outlined" onClick={() => console.log("cancelar")}>
+          Cancelar
+        </Button>
+      </Box>
     </Box>
   );
 }
